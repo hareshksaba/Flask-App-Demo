e90e865063b594263830b3921680f9a9bf238770
Haresh <hareshdevops@gmail.com>
2018-06-08 14:44:08 -0700

    Added second url

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/test')
+def hello_world():
+    return 'Hello Test User!'
+
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
 #test
